79ac616db9446f9b1ee932a69dab6514cf5cc72b
Niket Rauniyar <vishal@gmail.com>
2024-10-19 16:53:32 +0530

    small fix

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,6 +1,6 @@
 import Commerce from "@chec/commerce.js";
 
-const apiKey = 'pk_test_5793898652939dca6a53a8dbf24496de249a5dab71971';
+const apiKey = import.meta.env.VITE_CHEC_PUBLIC_KEY;
 
 export const commerce = new Commerce(apiKey, true);
 
